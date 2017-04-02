268ba56858f5f7295da183b50d0fe1042d4f421d
Santiago Rodriguez Parra <srodri31@eafit.edu.co>
2017-04-02 14:06:54 -0500

    distance between actual pos and another place according to some travelling mode

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,21 +1,24 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import { Geolocation } from 'ionic-native';
- 
+
 declare var google;
  
 @Component({
   selector: 'home-page',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
  
   @ViewChild('map') mapElement: ElementRef;
   map: any;
   labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; //label for the marker
   labelIndex = 0;
+  origin: any;
+  destination: any;
  
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
     
   }
  
@@ -28,12 +31,11 @@ export class HomePage {
     Geolocation.getCurrentPosition().then((position) => {
  
       let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
- 
+      this.origin = latLng;
       var pos = {
               lat: position.coords.latitude,
               lng: position.coords.longitude
       };
-
 
       let mapOptions = {
         center: latLng,
@@ -50,6 +52,7 @@ export class HomePage {
       //Add listener to add markers
       google.maps.event.addListener(this.map, 'click', (event) => {
         this.addMarker(event.latLng);
+        this.destination = event.latLng;
       });
 
     }, (err) => {
@@ -68,7 +71,7 @@ export class HomePage {
 
     });
   
-    let content = "<h4>Information!</h4>";          
+    let content = "Information!";          
   
     this.addInfoWindow(marker, content);
   
@@ -84,5 +87,74 @@ export class HomePage {
       infoWindow.open(this.map, marker);
     });
   
+  }
+
+  goTo(travelMode){
+    if(this.destination){
+      var service = new google.maps.DistanceMatrixService();
+      service.getDistanceMatrix(
+        {
+          origins: [this.origin],
+          destinations: [this.destination],
+          travelMode: travelMode,
+          avoidHighways: false,
+          avoidTolls: false,
+        }, (response, status) => {
+          this.parcingDistanceResults(response, status);
+        });
+    }else{
+      this.showAlert('First you have to mark a destination');
+    }
+  }
+
+  parcingDistanceResults(response, status){
+    if (status == google.maps.DistanceMatrixStatus.OK) {
+      var origins = response.originAddresses;
+      var destinations = response.destinationAddresses;
+
+      for (var i = 0; i < origins.length; i++) {
+        var results = response.rows[i].elements;
+        for (var j = 0; j < results.length; j++) {
+          var element = results[j];
+          var distance = element.distance.text;
+          var duration = element.duration.text;
+          var from = origins[i];
+          var to = destinations[j];
+          console.log('Distance:'+distance);
+        }
+      }
+    }
+
+  }
+
+  showAlert(message) {
+    let alert = this.alertCtrl.create({
+      title: 'Warning',
+      subTitle: message,
+      buttons: ['OK']
+    });
+    alert.present();
+  } 
+
+  selectKindTransport() {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Choose one');
+
+    var walk = {type: 'radio', label: 'walking', value: google.maps.TravelMode.WALKING, checked: true};
+    var drive = {type: 'radio', label: 'driving', value: google.maps.TravelMode.DRIVING};
+    var bicycle = {type: 'radio', label: 'bicycling', value: google.maps.TravelMode.WALKING};
+
+    alert.addInput(walk);
+    alert.addInput(drive);
+    alert.addInput(bicycle);
+
+    alert.addButton('Cancel');
+    alert.addButton({
+      text: 'OK',
+      handler: data => {
+        this.goTo(data);
+      }
+    });
+    alert.present();
   }
 }
